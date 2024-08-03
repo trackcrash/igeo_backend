533f75b3549807f3dfbb70399260a329498bdec0
trackcrash <auroknh@gmail.com>
2024-08-04 06:47:11 +0900

    jwt가 사용자이름 추출 성공적으로 처리하도록 변경

--- src/main/java/igeo/site/Provider/JwtTokenProvider.java
+++ src/main/java/igeo/site/Provider/JwtTokenProvider.java
@@ -1,8 +1,10 @@
 package igeo.site.Provider;
 
+import igeo.site.Auth.CustomUserDetailsService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -11,15 +13,19 @@ import org.springframework.stereotype.Component;
 
 import javax.validation.Valid;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 @Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
     @Value("${jwt.secret}")
     private String secretKey;
     @Value("${jwt.expiration}")
     private int expiration;
     private static final int refreshExpiration = 1000 * 60 * 60 * 24 * 7;
+    private final CustomUserDetailsService userDetailsService;
     public String generateToken(Authentication authentication)
     {
         Claims claims = Jwts.claims().setSubject(authentication.getName());
@@ -59,7 +65,22 @@ public class JwtTokenProvider {
 
     // 토큰에서 사용자 이름 추출
     public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
+        Claims claims = extractAllClaims(token);
+
+        // "authorities" 배열을 가져오기 위해 클레임에서 꺼내기
+        List<Map<String, Object>> authorities = (List<Map<String, Object>>) claims.get("authorities");
+
+        if (authorities != null && !authorities.isEmpty()) {
+            Map<String, Object> attributes = (Map<String, Object>) authorities.get(0).get("attributes");
+            if (attributes != null) {
+                String email = (String) attributes.get("email");
+                if (email != null) {
+                    return email;
+                }
+            }
+        }
+
+        throw new RuntimeException("Email not found in token");
     }
 
     // 토큰 만료 여부 확인
@@ -73,7 +94,7 @@ public class JwtTokenProvider {
     }
 
     public Authentication getAuthentication(String token) {
-        UserDetails userDetails = (UserDetails) extractAllClaims(token).get("authorities");
+        UserDetails userDetails = userDetailsService.loadUserByUsername(extractUsername(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 }
